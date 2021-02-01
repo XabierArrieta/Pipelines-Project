5eed47f6a933c056161c3be9cd31ad03f781dee9
XabierArrieta <xabierarrietachango@gmail.com>
2021-02-01 02:17:32 +0100

    updates

--- README.md
+++ README.md
@@ -1,24 +1,53 @@
-# Proyecto Pandas
+# Project: Data Pipeline - Madrid Airbnb
 
 ## Introducción:
 
+Airbnb es una compañía creada en 2008 por Brian Chesky, que ofrece una plataforma digital dedicada a la oferta de alojamientos a particulares y turísticos (alquiler vacacional) mediante la cual los anfitriones pueden publicitar y contratar el arriendo de sus propiedades con sus huéspedes; anfitriones y huéspedes pueden valorarse mutuamente, como referencia para futuros usuarios. El nombre es un acrónimo de airbed and breakfast.
+​
+Airbnb tiene una oferta de unas 2 000 000 propiedades en 192 países y 33 000 ciudades. 
+
+La ciudad de Madrid registró en 2019 la llegada de 10,4 millones de visitantes, de los cuales el 55% fueron internacionales y  el 45% nacionales. Se generaron 22,6 millones de pernoctaciones. 
+
+Los principales mercados emisores de turismo internacional fueron Estados Unidos, Italia, Francia, Reino Unido y Alemania y los que más crecieron, Estados Unidos, China, Italia, Brasil y México. Se alcanzó un gasto total de 10.451 millones y un gasto medio diario de 270 euros en la Comunidad de Madrid. 
 
 
+## Objetivo:
 
-## Librerias:
+Este es un proyecto que lo he realizado durante el Bootcamp de Data Anatlytics en Ironhack Maddrid.
 
-- pandas
-- numpy
-- matplotlib.pyplot
-- seaborn
-- re
+El objetivo es;
 
-## Técnicas:
+    -   Escoger una base de datos de [Kaggle](www.kaggle.com).
+    -   Limpiar la base de datos.
+    -   Enriquecerla, en mi caso,  a través de wev scraping.
+    -   Visualizar los datos obtenidos con gráficos.
 
-- null values
-- columns drop
-- duplicate data
-- string manipulation
+Las libreias usadas en este proyecto:  Os, Pandas, Sys, Requests, BeautifulSoup, Seaborn, matplotlib.pyplot.
+
+
+## Proceso:
+
+He escogido una base de datos de [Airbnb de Madrid Dataset](https://www.kaggle.com/rusiano/madrid-airbnb-data) con una usabilidad de 9,4 compuesto de 20837 filas y 16 columnas. En esta base de datos se muestran datos de los apartamentos que Airbnb tiene en la ciudad de Madrid en base a su ubicación, precio, número de comentarios entre otros.
+
+Los pasos a seguir;
+
+    -   Analizar el dataset; ya está bastante limpio por lo que reviso las columnas, quito los valores nulos, duplicados y las columnas no necesarias para mi análisis. 
+    -   Web scraping; una vez que tengo el dataset limpio decido enriquecerlo con información relacionada con los monumentos de interés cercanos a los apartamentos. 
+    -   Visualización: ----------------
+
 
 ## Conclusiones:
+
+-- Los barrios donde más apartamentos se ofrecen son:
+-- La mayoría de ellos están cerca de los principales puntos de interés de Madrid;
+-- Los que mejor puntuación obtienen se encuentran en:
+-- Los que menos puntuación obtienen se encuentran en:
+-- Los apartamentos más caros se encuentran en:
+-- Los apartamentos más baratos se encuentran en:
  
+## Enlaces:
+
+- [Kaggle](www.kaggle.com)
+- [Airbnb de Madrid Dataset](https://www.kaggle.com/rusiano/madrid-airbnb-data)
+- [Airbnb](https://www.airbnb.es/madrid-spain/stays) 
+- [Sitios de interés en Madrid](https://www.tripadvisor.es/Attractions-g187514-Activities-c47-Madrid.html)
--- src/cleaning_func.py
+++ src/cleaning_func.py
@@ -1,35 +1,91 @@
-#distrito.
-def dist(nombre):
-    if nombre == 'ChamartÃ\xadn':
-        return 'Chamartin'
-    elif nombre== 'TetuÃ¡n':
-        return 'Tetuan'
-    elif nombre == 'ChamberÃ\xad':
-        return 'Chamberí'
-    elif nombre == 'VicÃ¡lvaro':
-        return 'nombre'
+import os
+def download_dataset():
+    '''Downloads a dataset from kaggle and only keeps the csv in your data file. Beware of your own data structure:
+    this creates a data directory and also moves all the .csv files next to your jupyter notebooks to it.
+    Takes: url from kaggle
+    Returns: a folder with the downloaded csv
+    '''
     
-#barrios
-def carac(nombre):
-    if 'Ã¡' in nombre:
-         return nombre.replace('Ã¡','á')
-    elif 'Ã©' in nombre:
-         return nombre.replace('Ã©','é')
-    elif 'Ã\xad' in nombre: 
-         return nombre.replace('Ã\xad','í')
-    elif 'Ã³'in nombre:
-         return nombre.replace('Ã³','ó')
-    elif 'Ãº'in nombre:
-         return nombre.replace('Ãº', 'ú')
-    elif 'Ã¼' in nombre:
-         return nombre.replace('Ã¼','ü')
-    else:
-         return nombre
+    #Gets the name of the dataset.zip
+    url = input("Introduce la url: ")
+    
+    #Gets the name of the dataset.zip
+    endopint = url.split("/")[-1]
+    user = url.split("/")[-2]
+    
+    #Download, decompress and leaves only the csv
+    download = f"kaggle datasets download -d {user}/{endopint}; say -v Monica 'descargando'"
+    decompress = f"tar -xzvf {endopint}.zip; say -v Monica 'descomprimiendo'"
+    delete = f"rm -rf {endopint}.zip; say -v Monica 'borrando el zip'"
+    make_directory = "mkdir data"
+    lista = "ls >> archivos.txt"
+    
+    for i in [download, decompress, delete, make_directory, lista]:
+        os.system(i)
+    
+    #Gets the name of the csv (you should only have one csv when running this code)
+    lista_archivos = open('archivos.txt').read()
+    nueva = lista_archivos.split("\n")
+    
+    #Moves the .csv into the data directory
+    for i in nueva:
+        if i.endswith(".csv"):
+            move_and_delete = f"mv {i} data/dataset.csv; rm archivos.txt; say -v Monica 'moviendo el dataset'"
+            return os.system(move_and_delete)
+
+
+
+
+
+def cleaning_mad(mad):
+    
+    # Eliminar columnas no relevantes para el análisis.
+    
+    mad = .drop(['minimum_nights', 'last_review', 'reviews_per_month', 'calculated_host_listings_count', 'availability_365'], axis = 1 )
+    # Contar valores nulos.
+
+    mad.isnull().sum().sort_values(ascending=False)
+    
+    # Elimininar valores nulos.
+    
+    mad.dropna()
+    
+    # Eliminar valore duplicados.
+    
+    mad.drop_duplicates()
+    
+    # Mostrar distritos correctamente.
+
+    def dist(nombre):
+        if nombre == 'ChamartÃ\xadn':
+            return 'Chamartin'
+        elif nombre== 'TetuÃ¡n':
+            return 'Tetuan'
+        elif nombre == 'ChamberÃ\xad':
+            return 'Chamberí'
+        elif nombre == 'VicÃ¡lvaro':
+            return 'nombre'
+        
+    # Reemplazar caracteres especiales en los barrios.
+    
+    def carac(nombre):
+        if 'Ã¡' in nombre:
+             return nombre.replace('Ã¡','á')
+        elif 'Ã©' in nombre:
+             return nombre.replace('Ã©','é')
+        elif 'Ã\xad' in nombre: 
+             return nombre.replace('Ã\xad','í')
+        elif 'Ã³'in nombre:
+             return nombre.replace('Ã³','ó')
+        elif 'Ãº'in nombre:
+             return nombre.replace('Ãº', 'ú')
+        elif 'Ã¼' in nombre:
+             return nombre.replace('Ã¼','ü')
+        else:
+             return nombre
     
         
-        
-        
-        
+            return mad
         
         
         
