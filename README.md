76c832707e495c73e45cfe9ff87c5b3ebd60ef79
XabierArrieta <xabierarrietachango@gmail.com>
2021-02-02 00:42:41 +0100

    Pipelines Project

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Project: Data Pipeline - Madrid Airbnb
-[airbnb](airmad.jpg)
+
 ## Introducción:
 
 Airbnb es una compañía creada en 2008 por Brian Chesky, que ofrece una plataforma digital dedicada a la oferta de alojamientos a particulares y turísticos (alquiler vacacional) mediante la cual los anfitriones pueden publicitar y contratar el arriendo de sus propiedades con sus huéspedes; anfitriones y huéspedes pueden valorarse mutuamente, como referencia para futuros usuarios. El nombre es un acrónimo de airbed and breakfast.
 ​
-Airbnb tiene una oferta de unas 2 000 000 propiedades en 192 países y 33 000 ciudades. 
+Airbnb tiene una oferta de unas 2 000 000 propiedades en 192 países y 33 000 ciudades. En España aterrizó en el año 2008.
 
-La ciudad de Madrid registró en 2019 la llegada de 10,4 millones de visitantes, de los cuales el 55% fueron internacionales y  el 45% nacionales. Se generaron 22,6 millones de pernoctaciones. 
+En 2019, la ciudad de Madrid registró en la llegada de 10,4 millones de visitantes, de los cuales el 55% fueron internacionales y el 45% nacionales. Se generaron 22,6 millones de pernoctaciones. 
 
 Los principales mercados emisores de turismo internacional fueron Estados Unidos, Italia, Francia, Reino Unido y Alemania y los que más crecieron, Estados Unidos, China, Italia, Brasil y México. Se alcanzó un gasto total de 10.451 millones y un gasto medio diario de 270 euros en la Comunidad de Madrid. 
 
@@ -19,10 +19,10 @@ El objetivo es;
 
 -   Escoger una base de datos de [Kaggle](www.kaggle.com).
 -   Limpiar la base de datos.
--   Enriquecerla, en mi caso,  a través de wev scraping.
+-   Enriquecerla, a través de wev scraping o Api.
 -   Visualizar los datos obtenidos con gráficos.
 
-Las libreias usadas en este proyecto:  Os, Pandas, Sys, Requests, BeautifulSoup, Seaborn, matplotlib.pyplot.
+Las libreias usadas en este proyecto: Pandas, Sys, Requests, BeautifulSoup, Geocoder,Seaborn, matplotlib.pyplot.
 
 
 ## Proceso:
@@ -31,19 +31,17 @@ He escogido una base de datos de [Airbnb de Madrid Dataset](https://www.kaggle.c
 
 Los pasos a seguir;
 
--   Analizar el dataset; ya está bastante limpio por lo que reviso las columnas, quito los valores nulos, duplicados y las columnas no necesarias para mi análisis. 
--   Web scraping; una vez que tengo el dataset limpio decido enriquecerlo con información relacionada con los monumentos de interés cercanos a los apartamentos. 
--   Visualización: ----------------
+-   Analizar el dataset; ya está bastante limpio por lo que reviso las columnas, quito los valores nulos, duplicados y las columnas no necesarias para mi análisis. Además cambio los caracteres especiales, cómo los acentos.
+-   Web scraping; una vez que tengo el dataset limpio decido enriquecerlo con información relacionada con los monumentos de interés cercanos a los apartamentos. Además, con Geocoder, consigo añadir la longitud y latitud y calcular la distancia al monumento más cercano.
+-   Visualización: Observando los precios, distritos, barrios y monumentos, realizo diferentes gráficos.
 
 
 ## Conclusiones:
 
-- Los barrios donde más apartamentos se ofrecen son:
-- La mayoría de ellos están cerca de los principales puntos de interés de Madrid:
-- Los que mejor puntuación obtienen se encuentran en:
-- Los que menos puntuación obtienen se encuentran en:
-- Los apartamentos más caros se encuentran en:
-- Los apartamentos más baratos se encuentran en:
+- Los barrios donde más apartamentos se ofrecen son: Centro, Salamanca y Chamberí.
+- La mayoría de ellos están cerca de los principales puntos de interés de Madrid: Malasaña, Barrio de las Letras y el Estadio Santiago Bernabeu.
+- ¿Quieres saber cuanto cuesta el apartamento más caro? y, ¿el más barato?
+Te invito a que lo descubras tu mismo.
  
 ## Enlaces:
 
--- src/cleaning_func.py
+++ src/cleaning_func.py
@@ -1,38 +1,13 @@
-import os
-def download_dataset():
-    '''Downloads a dataset from kaggle and only keeps the csv in your data file. Beware of your own data structure:
-    this creates a data directory and also moves all the .csv files next to your jupyter notebooks to it.
-    Takes: url from kaggle
-    Returns: a folder with the downloaded csv
-    '''
-    
-    #Gets the name of the dataset.zip
-    url = input("Introduce la url: ")
-    
-    #Gets the name of the dataset.zip
-    endopint = url.split("/")[-1]
-    user = url.split("/")[-2]
-    
-    #Download, decompress and leaves only the csv
-    download = f"kaggle datasets download -d {user}/{endopint}; say -v Monica 'descargando'"
-    decompress = f"tar -xzvf {endopint}.zip; say -v Monica 'descomprimiendo'"
-    delete = f"rm -rf {endopint}.zip; say -v Monica 'borrando el zip'"
-    make_directory = "mkdir data"
-    lista = "ls >> archivos.txt"
-    
-    for i in [download, decompress, delete, make_directory, lista]:
-        os.system(i)
-    
-    #Gets the name of the csv (you should only have one csv when running this code)
-    lista_archivos = open('archivos.txt').read()
-    nueva = lista_archivos.split("\n")
-    
-    #Moves the .csv into the data directory
-    for i in nueva:
-        if i.endswith(".csv"):
-            move_and_delete = f"mv {i} data/dataset.csv; rm archivos.txt; say -v Monica 'moviendo el dataset'"
-            return os.system(move_and_delete)
-
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import json
+import statistics 
+import src.cleaning_func as cf
+import sys
+sys.path.append("../")
+import requests
+from bs4 import BeautifulSoup
 
 
 
@@ -41,7 +16,7 @@ def cleaning_mad(mad):
     
     # Eliminar columnas no relevantes para el análisis.
     
-    mad = .drop(['minimum_nights', 'last_review', 'reviews_per_month', 'calculated_host_listings_count', 'availability_365'], axis = 1 )
+    mad = mad.drop(['minimum_nights', 'last_review', 'reviews_per_month', 'calculated_host_listings_count', 'availability_365'], axis = 1 )
     # Contar valores nulos.
 
     mad.isnull().sum().sort_values(ascending=False)
@@ -56,39 +31,75 @@ def cleaning_mad(mad):
     
     # Mostrar distritos correctamente.
 
-    def dist(nombre):
-        if nombre == 'ChamartÃ\xadn':
-            return 'Chamartin'
-        elif nombre== 'TetuÃ¡n':
-            return 'Tetuan'
-        elif nombre == 'ChamberÃ\xad':
-            return 'Chamberí'
-        elif nombre == 'VicÃ¡lvaro':
-            return 'nombre'
+def dist(nombre):
+    if nombre == 'ChamartÃ\xadn':
+        return 'Chamartin'
+    elif nombre== 'TetuÃ¡n':
+        return 'Tetuan'
+    elif nombre == 'ChamberÃ\xad':
+        return 'Chamberí'
+    elif nombre == 'VicÃ¡lvaro':
+        return 'nombre'
         
     # Reemplazar caracteres especiales en los barrios.
     
-    def carac(nombre):
-        if 'Ã¡' in nombre:
-             return nombre.replace('Ã¡','á')
-        elif 'Ã©' in nombre:
-             return nombre.replace('Ã©','é')
-        elif 'Ã\xad' in nombre: 
-             return nombre.replace('Ã\xad','í')
-        elif 'Ã³'in nombre:
-             return nombre.replace('Ã³','ó')
-        elif 'Ãº'in nombre:
-             return nombre.replace('Ãº', 'ú')
-        elif 'Ã¼' in nombre:
-             return nombre.replace('Ã¼','ü')
-        else:
-             return nombre
+def carac(nombre):
+    if 'Ã¡' in nombre:
+        return nombre.replace('Ã¡','á')
+    elif 'Ã©' in nombre:
+        return nombre.replace('Ã©','é')
+    elif 'Ã\xad' in nombre: 
+        return nombre.replace('Ã\xad','í')
+    elif 'Ã³'in nombre:
+        return nombre.replace('Ã³','ó')
+    elif 'Ãº'in nombre:
+        return nombre.replace('Ãº', 'ú')
+    elif 'Ã¼' in nombre:
+        return nombre.replace('Ã¼','ü')
+    else:
+        return nombre
     
-        
-            return mad
-        
-        
-        
-        
-        
-        
+    
+ 
+
+def scraping(mad_ready):
+    #Abrir la database
+    mad.to_csv("mad_for_vis.csv", encoding='utf-8-sig') 
+    #poner url
+    url_mad = "https://www.tripadvisor.es/Attractions-g187514-Activities-c47-Madrid.html"
+    response = requests.get(url_mad)
+    response
+    soup = BeautifulSoup(response.content)
+    #saca los titulos
+    mad_att = soup.select("h2")
+    #quita espacios
+    mad_att[0].text.strip()
+    #aplica función
+    att = [(i.text.strip()) for i in mad_att]
+    #crea nuevo dataframe
+    att = pd.DataFrame(att, columns=['attractions'])
+    # uso geocoder
+    geocoder.osm("Palacio Real de Madrid, Madrid").json
+    
+def lat_long():   
+    lat = []
+    long = []
+    for m in monuments["name"]:
+        data = geocoder.osm(f"{m}, Madrid").json
+        if data:
+            lat.append(data.get('lat', None))
+            long.append(data.get('lng', None))
+        else:
+            lat.append(None)
+            long.append(None)
+    
+def dist_mon():
+for  i, fila in mad.iterrows():
+    lista = [fila["latitude"],fila["longitude"]]#muestra las filas con la latitud y longitud
+    print(monuments.loc[monuments.apply(lambda row: haversine(lista,[row["latitude"],row["longitude"]]), axis=1).argmin()]["name"])#muestra la serie con la distancia en km con el loc accedemos al monumento más cercano y si ponemos el nombre nos dice el ordén del monumenrto más cercano.    
+
+def cercano():
+closest = []
+for  i, fila in mad.iterrows():
+    lista = [fila["latitude"],fila["longitude"]]#muestra las filas con la latitud y longitud
+    closest.append(monuments.loc[monuments.apply(lambda row:haversine(lista,[row["latitude"],row["longitude"]]), axis=1).argmin()]["name"])   
